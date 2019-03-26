0c5854d5d7fc4077b85bae460c925bc8fe9b3b15
rojaswestall <rojaswestall@gmail.com>
2019-03-26 18:54:54 -0400

    finished submitter

--- README.md
+++ README.md
@@ -1,2 +1,148 @@
 # service-hour-submitter
-Script to automatically submit service hours from google sheets spreadsheet to myodphi
+This is a script to automatically submit service hours from the midwest template for service hours as a google sheet spreadsheet to myodphi. Alpha Alpha finds it easier to keep track of service in a spreadsheet, so this script was written so we don't have to manually transfer the hours over to the site at the end of each quarter. This script may not work with newer iterations of the site. The last time this script was tested was in March of 2019.
+
+### Requirements
+Install Python 3
+[Download for Mac](https://docs.python-guide.org/starting/install3/osx/#install3-osx)
+[Download for Windows](https://docs.python-guide.org/starting/install3/win/#install3-windows)
+[Download for Linux](https://docs.python-guide.org/starting/install3/linux/#install3-linux)
+
+Once installed, clone or download this project, then navigate to where you cloned/downloaded the project and run the following commands:
+
+```
+pip3 install requests bs4 json
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+
+Everything that is showing up in the command line is telling you that it is downloading and installing python modules. It is not breaking your computer : )
+
+
+### Usage
+In `config.json`, enter all of the following information:
+- **username** used for myodphi (i.e. "odphi_username": "rojaswestall@gmail.com")
+- **password** used for myodphi (i.e. "odphi_password": "password1234")
+- **region** your chapter is in (i.e. "region": "Midwest")
+- **school** (i.e. "school": "Northwestern University")
+- **semester** of service (i.e. "semester": "Winter")
+- **year** of service (i.e. "year": "2019")
+- **name** as it appears on the chapter service sheet (i.e. "name_on_service_sheet": "Gabriel Rojas-Westall")
+- **ID of the google sheet**. This can be found in the URL of the service spreadsheet. In the example below, it's the portion with all of the x's:
+
+https://docs.google.com/spreadsheets/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/edit#gid=0123456789
+
+(i.e. "sheets_id": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+**Note: The region, school, semester, and year must be exactly as you would normally submit it through the service hours page on myopdhi. You can look at the options section below to see how they are listed exactly on the site**
+
+**It's important that you don't modify anything above odphi_username in the config file!**
+
+From terminal, run the following command and you're done!
+```
+python3 submitter.py
+```
+
+Check to make sure that all the hours were submitted by getting a report of your hours on myodphi!
+
+
+### Assumptions
+- The hours for each individual bro are stored in a tab called "Hours"
+- The dates of the service hours take the format "mm/dd/yyyy"
+- The format of the sheet follows this [template](https://docs.google.com/spreadsheets/d/1dkmCiNWbt00yAwdC3qgnSNqTY2I8pQ9Vt1f3GGHy5Vo/edit#gid=1403236818)
+
+
+### Options
+The following options are available for a user to select from on the myodphi site. You should choose from the following and copy your choice *exactly* into the `config.json` file.
+
+Region:
+- Central Plains
+- Central Texas
+- East Texas
+- Midwest
+- Northwest
+- North Texas
+- Pacific
+- Southeast
+- Southwest
+
+School:
+- Auburn University
+- Arizona State University
+- Baylor University
+- California State University - Bakersfield
+- California State University - Monterey Bay
+- California State University - Dominguez Hills
+- California State University - Fresno
+- California State University - Stanislaus
+- Colorado State University - Ft. Collins
+- Colorado State University - Pueblo
+- Eastern Washington University
+- Fresno State University
+- Heritage University
+- Kansas State University
+- Michigan State University
+- Northeastern Illinois University
+- Northern Arizona University
+- Northwestern University
+- Oklahoma State University
+- Oregon State University
+- Our Lady of the Lake University
+- Portland State University
+- Prairie View A&M University
+- Sam Houston State University
+- Southern Illinois University
+- Southern Methodist University
+- St. Mary's University
+- Stephen F. Austin State University
+- Texas A&M - Corpus Christi
+- Texas A&M - International
+- Texas A&M - Kingsville
+- Texas A&M - Commerce
+- Texas A&M University
+- Texas A&M University - San Antonio
+- Texas Christian University
+- Texas Southern University
+- Texas State University
+- Texas Tech University
+- University of Arizona
+- University of California - Merced
+- University of Central Oklahoma
+- University of Florida
+- University of Houston
+- University of Idaho
+- University of Illinois - Chicago
+- University of Illinois - Urbana-Champaign
+- University of Incarnate Word
+- University of Nevada - Las Vegas
+- University of Nevada - Reno
+- University of New Mexico
+- University of North Texas
+- University of Oklahoma
+- University of Texas - Arlington
+- University of Texas - Austin
+- University of Texas - Dallas
+- University of Texas - El Paso
+- University of Texas - Pan American
+- University of Texas - San Antonio
+- University of the Pacific
+- University of Utah
+- University of Washington
+- University of West Florida
+- University of Wisconsin - Milkwaukee
+- University of Wisconsin - Oshkosh
+- University of Wisconsin - Parkside
+- Washington State University
+- West Texas A&M University
+- Western Michigan University
+- Western Oregon University
+
+Semester:
+- Fall
+- Winter
+- Spring
+
+Year:
+- 2018
+- 2019
+- 2020
+- 2021
+- 2022
---
+++ submitter.py
@@ -0,0 +1,203 @@
+# For google
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# For ODPhi site
+import requests
+from bs4 import BeautifulSoup
+
+# For development 
+import json
+import struct
+
+# Global Variables
+with open('config.json') as json_file:  
+    config = json.load(json_file)["installed"]
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = config["sheets_id"]
+RANGE_NAME = 'Hours!A:Z'
+
+# Could be done a lot better, this is just a quick fix
+def dateFormatter(date):
+    d_m_y = date.split("/")
+
+    if len(d_m_y[0]) == 1:
+        d_m_y[0] = "0" + d_m_y[0] + "/"
+    else:
+        d_m_y[0] = d_m_y[0] + "/"
+
+    if len(d_m_y[1]) == 1:
+        d_m_y[1] = "0" + d_m_y[1] + "/"
+    else:
+        d_m_y[1] = d_m_y[1] + "/"
+    
+    date = ""
+    for n in d_m_y:
+        date += n
+
+    return date
+
+def main():
+
+    ##########################################
+    ###### GOOGLE SHEETS AUTHENTICATION ######
+    ##########################################
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'config.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    # ##########################################
+    # ######### CALL GOOGLE SHEETS API #########
+    # ##########################################
+
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found was found in the google sheet. Please make sure the ID that was provided for the service sheet in config.json is correct and that you have access to the sheet.')
+    else:
+        # Constructing the service object for user
+        service_hours = []
+        for i, row in enumerate(values):
+            if (i == 1):
+                for date in row[4:]:
+                    service_hours.append({"date": date})
+            elif (i == 2):
+                for j, event in enumerate(row[4:]):
+                    service_hours[j]["event"] = event
+            elif (row[1] == config["name_on_service_sheet"]):
+                offset = 0
+                for j, hours in enumerate(row[4:]):
+                    if hours != "0":
+                        service_hours[j - offset]["hours"] = float(hours)
+                    else:
+                        del service_hours[j - offset]
+                        offset += 1
+
+        # Example of what the data might look like:
+        # service_hours = [   
+        #     {'date': '1/19/2019', 'event': 'FSL: Recruitment & Support', 'hours': 1.0},
+        #     {'date': '1/19/2019', 'event': 'SHPE Jr', 'hours': 2.5},
+        #     {'date': '1/21/2019', 'event': 'MLKDOS', 'hours': 3.0},
+        #     {'date': '1/26/2019', 'event': 'SHPE Jr', 'hours': 2.5},
+        #     {'date': '2/2/2019', 'event': 'SHPE Jr', 'hours': 2.0},
+        #     {'date': '2/9/2019', 'event': 'SHPE Jr', 'hours': 2.0},
+        #     {'date': '2/16/2019', 'event': 'SHPE Jr', 'hours': 1.0},
+        #     {'date': '2/23/2019', 'event': 'EPL', 'hours': 2.0},
+        #     {'date': '2/23/2019', 'event': 'SHPE Jr', 'hours': 2.0},
+        #     {'date': '3/2/2019', 'event': 'SHPE Jr', 'hours': 2.0},
+        #     {'date': '3/9/2019', 'event': 'SHPE Jr Competition', 'hours': 8.0}
+        # ]
+
+        ##########################################
+        ############ LOGIN TO MYODPHI ############
+        ##########################################
+
+        login_url = 'https://myodphi.com/login/'
+        login_action = 'https://myodphi.com/wp-login.php'
+
+        # Start session
+        session = requests.session()
+
+        # Not entirely sure, but I think we need this get request for the cookies and headers
+        r = session.get(login_url)
+
+        # Set username and password as the form wants it
+        payload = {
+            "log":config["odphi_username"],
+            "pwd":config["odphi_password"]
+        }
+
+        # Login
+        r = session.post(login_action, data = payload)
+
+        ###########################################
+        ######### SUBMIT HOURS TO MYODPHI #########
+        ###########################################
+
+        service_url = 'https://myodphi.com/servicehours/'
+
+        # Navigate to service page and get basic info
+        r = session.get(service_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        
+        official_name = soup.find("input", id="input_5_1")["value"]
+        official_email = soup.find("input", id="input_5_2")["value"]
+        official_username = soup.find("input", id="input_5_3")["value"]
+
+        is_submit = soup.find("input", {"name":"is_submit_5"})["value"]
+        gform_submit = soup.find("input", {"name":"gform_submit"})["value"]
+        state5 = soup.find("input", {"name":"state_5"})["value"]
+        gform_target = soup.find("input", {"name":"gform_target_page_number_5"})["value"]
+        gform_source = soup.find("input", {"name":"gform_source_page_number_5"})["value"]
+
+        # Submit all hours, making a get request for a new form for each event
+        for service_event in service_hours:
+            r = session.get(service_url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            
+            _gform_submit_nonce = soup.find("input", {"name":"_gform_submit_nonce_5"})["value"]
+            unique_id = soup.find("input", {"name":"gform_unique_id"})["value"]
+            gform_field = soup.find("input", {"name":"gform_field_values"})["value"]
+            
+            service_details = [
+                ("input_11", (None, official_username)),
+                ("input_1", (None, official_name)),
+                ("input_12", (None, official_email)),
+                ("input_2", (None, official_email)),
+                ("input_3", (None, official_username)),
+                ("input_4", (None, config["region"])),
+                ("input_5", (None, config["school"])),
+                ("input_6", (None, config["semester"])),
+                ("input_7", (None, config["year"])),
+                ("input_8", (None, dateFormatter(service_event["date"]))),
+                ("input_9", (None, str(service_event["hours"]))),
+                ("input_10", (None, service_event["event"])),
+                ("_gform_submit_nonce_5", (None, _gform_submit_nonce)),
+                ("_wp_http_referer", (None, "/servicehours/")),
+                ("is_submit_5", (None, is_submit)),
+                ("gform_submit", (None, gform_submit)),
+                ("gform_unique_id", (None, unique_id)),
+                ("state_5", (None, state5)),
+                ("gform_target_page_number_5", (None, gform_target)),
+                ("gform_source_page_number_5", (None, gform_source)),
+                ("gform_field_values", (None, gform_field))
+            ]
+
+            r = session.post(service_url, files = service_details)
+            print("\n\nThe following has been submitted:")
+            print("  Event: ", service_event["event"])
+            print("  Date: ", service_event["date"])
+            print("  Hours: ", service_event["hours"])
+        
+        print("All done submitting! Check the service report page to make sure they're all there!")
+
+if __name__ == '__main__':
+    main()
